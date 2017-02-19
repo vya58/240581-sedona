4ff6ec6d519c2c92f26e9c0d30a80d072e29d7bd
vya58 <vya58@yandex.ru>
2017-02-19 23:05:27 +0300

    Поправил js, чтобы при его подключении форма поиска гостиницы скрывалась, а без него - по умолчанию - открыта.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 var link = document.querySelector(".start-search")
 var fieldset = document.querySelector(".search-hotels");
 
+fieldset.classList.toggle("search-hotels-show");
+
 link.addEventListener("click", function(event) {
         event.preventDefault();
         fieldset.classList.toggle("search-hotels-show");
